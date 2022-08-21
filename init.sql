22404422978b6b6d89d09d5baa2eeca039c49aef
Vedblade <18503283+yad439@users.noreply.github.com>
2022-08-21 13:39:31 +0000

    Add indices to sql init script

--- init.sql
+++ init.sql
@@ -7,10 +7,15 @@ create table packages (
 create table files (
   path text not null,
   name text not null,
-  package_name text not null
-  --foreign key package_id references packages (id)
+  package_name text not null,
+  foreign key(package_name) references packages(name)
 );
 create table dependencies (
   file text not null,
   dependency text not null
 );
+
+create index idx_files_package_name on files (package_name);
+create index idx_files_path on files (path);
+create unique index idx_packages_name on packages (name);
+create index idx_dependencies_file on dependencies (file);
