20c48913aac0f75ffdb114a10272bf25a9f0db4f
Vedblade <18503283+yad439@users.noreply.github.com>
2022-08-21 13:19:56 +0000

    Add repository tests

--- test/package_repository_test.exs
+++ test/package_repository_test.exs
@@ -3,7 +3,8 @@ defmodule PackageRepositoryTest do
   alias Exqlite.Sqlite3
 
   setup do
-    {:ok, conn} = Sqlite3.open(":memory:")
+    repository = start_supervised!({PackageRepository, ":memory:"})
+    conn = :sys.get_state(repository)
 
     :ok =
       Sqlite3.execute(
@@ -23,19 +24,242 @@ defmodule PackageRepositoryTest do
         "create table dependencies (file text not null, dependency text not null)"
       )
 
-    %{conn: conn}
+    %{repository: repository}
   end
 
-  test ":add handler adds packages", %{conn: conn} do
+  test "add_packages", %{repository: repo} do
     packages = [
       %{name: "package1", version: "1.0.0", path: "path/to/package1"},
       %{name: "package2", version: "0.5", path: "path/to/package2"}
     ]
 
-    res = PackageRepository.handle_cast({:add, packages}, conn)
+    res = PackageRepository.add_packages(repo, packages)
+    conn = :sys.get_state(repo)
 
-    assert res == {:noreply, conn}
-    assert execute_select(conn, "select name from packages") == [["package1"], ["package2"]]
+    assert res == :ok
+
+    assert MapSet.new(execute_select(conn, "select name from packages")) ==
+             MapSet.new([["package1"], ["package2"]])
+  end
+
+  test "delete_package", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package1', '1.0.0', 'path/to/package1', 1)"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package2', '0.5', 'path/to/package2', 1)"
+      )
+
+    res = PackageRepository.delete_package(repo, "package1")
+    _ = :sys.get_state(repo)
+
+    assert res == :ok
+    assert execute_select(conn, "select name from packages") == [["package2"]]
+  end
+
+  test "update_files", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package1', '1.0.0', 'path/to/package1', 1)"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package2', '0.5', 'path/to/package2', 1)"
+      )
+
+    :ok = Sqlite3.execute(conn, "insert into files values ('path/to/file1', 'file1', 'package1')")
+    :ok = Sqlite3.execute(conn, "insert into files values ('path/to/file2', 'file2', 'package1')")
+    :ok = Sqlite3.execute(conn, "insert into files values ('path/to/file3', 'file3', 'package2')")
+
+    res = PackageRepository.update_files(repo, "package1", ["path/to/file4", "path/to/file5"])
+    _ = :sys.get_state(repo)
+
+    assert res == :ok
+
+    assert MapSet.new(execute_select(conn, "select name from files")) ==
+             MapSet.new([["file3"], ["file4"], ["file5"]])
+  end
+
+  test "set_outdated", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package1', '1.0.0', 'path/to/package1', 0)"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package2', '0.5', 'path/to/package2', 0)"
+      )
+
+    result =
+      PackageRepository.set_outdated(repo, %{
+        name: "package1",
+        version: "1.1.0",
+        path: "path/to/package1"
+      })
+
+    _ = :sys.get_state(repo)
+
+    assert result == :ok
+
+    assert execute_select(
+             conn,
+             "select version, needs_update from packages where name = 'package1'"
+           ) == [["1.1.0", 1]]
+  end
+
+  test "get_versions", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package1', '1.0.0', 'path/to/package1', 1)"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package2', '0.5', 'path/to/package2', 1)"
+      )
+
+    result = PackageRepository.get_versions(repo)
+
+    assert result == %{"package1" => "1.0.0", "package2" => "0.5"}
+  end
+
+  test "get_outdated", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package1', '1.0.0', 'path/to/package1', 1)"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package2', '0.5', 'path/to/package2', 0)"
+      )
+
+    result = PackageRepository.get_outdated(repo)
+
+    assert result == [%{name: "package1", version: "1.0.0"}]
+  end
+
+  test "get_packages_for_files", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package1', '1.0.0', 'path/to/package1', 1)"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into packages values ('package2', '0.5', 'path/to/package2', 0)"
+      )
+
+    :ok = Sqlite3.execute(conn, "insert into files values ('usr/bin/file1', 'file1', 'package1')")
+    :ok = Sqlite3.execute(conn, "insert into files values ('usr/bin/file2', 'file2', 'package1')")
+    :ok = Sqlite3.execute(conn, "insert into files values ('usr/bin/file3', 'file3', 'package2')")
+
+    result = PackageRepository.get_packages_for_files(repo, ["file1", "file2", "file3"])
+
+    assert result == [
+             %{
+               name: "package1",
+               path: "path/to/package1",
+               files: [
+                 %{name: "file1", path: "usr/bin/file1"},
+                 %{name: "file2", path: "usr/bin/file2"}
+               ]
+             },
+             %{
+               name: "package2",
+               path: "path/to/package2",
+               files: [%{name: "file3", path: "usr/bin/file3"}]
+             }
+           ]
+  end
+
+  test "add_dependencies", %{repository: repo} do
+    result = PackageRepository.add_dependencies(repo, "file1", ["file2", "file3"])
+    assert result == :ok
+
+    conn = :sys.get_state(repo)
+
+    assert execute_select(conn, "select * from dependencies") == [
+             ["file1", "file2"],
+             ["file1", "file3"]
+           ]
+  end
+
+  test "remove_dependency", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into dependencies values ('file1', 'file2')"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into dependencies values ('file1', 'file3')"
+      )
+
+    result = PackageRepository.remove_dependency(repo, "file1", "file2")
+    assert result == :ok
+
+    _ = :sys.get_state(repo)
+
+    assert execute_select(conn, "select * from dependencies") == [["file1", "file3"]]
+  end
+
+  test "get_dependencies", %{repository: repo} do
+    conn = :sys.get_state(repo)
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into dependencies values ('file1', 'file2')"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into dependencies values ('file1', 'file3')"
+      )
+
+    :ok =
+      Sqlite3.execute(
+        conn,
+        "insert into dependencies values ('file4', 'file5')"
+      )
+
+    result = PackageRepository.get_dependencies(repo, ["file1", "file4"])
+    assert Enum.to_list(result) == ["file2", "file3", "file5"]
   end
 
   defp execute_select(conn, query) do
